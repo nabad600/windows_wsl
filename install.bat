afb595ad084b9800b4925bef83c8b14890dc7e50
Naba Kumar Das <nabad600@gmail.com>
2021-11-30 15:33:48 +0530

    Update install.bat

--- install.bat
+++ install.bat
@@ -3,9 +3,8 @@ ECHO Please Wait.... WSL/WSL2 download
 # Package checking, Download and Install Multipass package
 @ECHO OFF
 ECHO Please Wait.... WSL/WSL2 download
-    DISM /online /enable-feature /featurename:HypervisorPlatform -All
-    DISM /online /enable-feature /featurename:VirtualMachinePlatform -All
-    DISM /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux -All
-    curl -L -C - https://github.com/nabad600/windows_wsl/releases/download/v1.0.0/deck-app.tar --output %temp%\deck-app.tar
-    wsl --import Deck-app %temp%\deck-app %temp%\deck-app.tar
-    
+DISM /online /enable-feature /featurename:VirtualMachinePlatform -All
+DISM /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux -All
+curl -L -C - https://github.com/nabad600/windows_wsl/releases/download/v1.0.0/Deck-app.tar --output %temp%\Deck-app.tar
+wsl --import deck-app C:\deck-app %temp%\Deck-app.tar
+wsl --set-version deck-app 2
