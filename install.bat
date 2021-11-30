2fbe725999638d42e4a5e118ccabc3a3b7bc2ae6
Naba Kumar Das <nabad600@gmail.com>
2021-11-30 16:17:35 +0530

    Update install.bat

--- install.bat
+++ install.bat
@@ -8,3 +8,4 @@ DISM /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux -All
 curl -L -C - https://github.com/nabad600/windows_wsl/releases/download/v1.0.0/Deck-app.tar --output %temp%\Deck-app.tar
 wsl --import deck-app C:\deck-app %temp%\Deck-app.tar
 wsl --set-version deck-app 2
+https://drive.google.com/file/d/1zF-SQmb1Wk7GmK6j09Edim0Pa4b67Mmr/view?usp=sharing
