ec367ed77f1d643bc801f854f41981b52c5e807f
Naba Kumar Das <nabad600@gmail.com>
2022-03-23 11:46:49 +0530

    Update install_dev.bat

--- install_dev.bat
+++ install_dev.bat
@@ -1,46 +1,37 @@
-@Echo off
-set BuildNumber=1902
-set Linuxverstion=Linux
-for /f "usebackq delims== tokens=2" %%x in (`wmic os get BuildNumber /format:value`) do set CurrentBuildNumber=%%x
-Echo CurrentBuildNumber="%CurrentBuildNumber%"
-If %CurrentBuildNumber% GTR %BuildNumber% (
-    Echo Are you Eligible
-    Echo Checking for Windows Subsystem for Linux...
-    @For /F %%A IN ('dism /online /get-featureinfo /featurename:VirtualMachinePlatform^|find "Enabled" /C'
-    ) Do @If %%A == 0 (
-        DISM /online /NoRestart /enable-feature /featurename:VirtualMachinePlatform -All
-    ) Else (
-        Echo ...Virtual Machine Platform already installed.
-    )
-    @For /F %%A IN ('dism /online /get-featureinfo /featurename:Microsoft-Windows-Subsystem-Linux^|find "Enabled" /C'
-    ) Do @If %%A == 0 (
-        DISM /online /NoRestart /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux -All
-    ) Else (
-        Echo ...Windows Subsystem for Linux already installed.
-    )
-    @For /F %%A IN ('wsl -d deck-app uname -a'
-    ) Do @If %%A == Linux (
-        Echo Already there Distribution in your system.
-    ) Else (
-        Echo
-        curl -L -C - https://github.com/nabad600/windows_wsl/releases/download/v1.0.1/Deck-app.tar --output Deck-app.tar
-        wsl --import deck-app C:\deck-app D:\custom_os\Deck-app.tar
-        wsl --set-version deck-app 2
-        del Deck-app.tar
-    )
-    Echo ...Checking and Downloading WSL2 Kernel Update.
-    @For /F %%A IN ('wsl -l -v^|find "1" /C'
-    ) Do @If %%A == 0 (
-        Echo ... Your system already WSL2 install
-    ) Else (
-        curl -L -C - https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi --output wsl_update_x64.msi
-        msiexec /i "wsl_update_x64.msi" /passive
-        SLEEP 5
-        del wsl_update_x64.msi
-        wsl --set-default-version 2
-        wsl --set-version deck-app 2
-    )
-) Else (
-    Echo This PC doesn't meet the system requirements to upgrade your system minimum BuildNumber 1093
-)
-pause
+@echo off
+
+rem Make sure script is run with admin privileges
+
+for /f "delims=" %%F in ('powershell -C "(New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)"') do set admin=%%F
+
+if %admin%==False (
+	echo Script needs to run as admin, please relaunch
+	PAUSE
+	exit
+) else (
+	echo You are running as Administrator
+) 
+
+rem Enable Optional Windows Feature Microsoft-Windows-Subsystem-Linux
+
+for /f "delims=" %%F in ('powershell -C "(Get-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux).State"') do set wslenabled=%%F
+
+if %wslenabled%==False (
+	echo Microsoft-Windows-Subsystem-Linux not enabled. Enabling now. Restart required
+	powershell -C "Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux"
+	PAUSE
+	exit
+) else (
+	echo Microsoft-Windows-Subsystem-Linux is enabled
+) 
+
+ECHO Setting WSLENV ...
+cmd.exe /c setx WSLENV USERNAME:USERPROFILE/p:DOCKER_HOST
+setx WSLENV USERNAME:USERPROFILE/p:DOCKER_HOST
+ECHO ... WSLENV is now %WSLENV%
+
+
+echo Installation is now complete
+
+PAUSE
+exit
